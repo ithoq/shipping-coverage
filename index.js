52fb61fd33f3732c0bbe2ae6a87755fa8d8240de
Joridos <joridoss@gmail.com>
2016-11-20 20:18:27 -0200

    added n4j env

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ var express = require('express'),
 	neo4j = require('neo4j-driver').v1,
 	app = express()
 
+app.set('port', (process.env.PORT || 5000));
 
 // view engine
 app.set('views', path.join(__dirname, 'views'));
@@ -16,6 +17,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(require('./controllers'))
 
-app.listen(80, function() {
+app.listen(app.get('port'), function() {
   console.log('Listening on port 3000...')
 })
